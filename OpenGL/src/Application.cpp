fbebf42193821ec78c7cd3178f94f6fe5967d97b
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2022-05-29 14:00:29 +1000

    9. Index buffers. glDrawElements

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -133,10 +133,16 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
+    float positions[] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
          0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
+    };
+
+    unsigned indices[] = {
+        0, 1, 2,
+        2, 3, 0,
     };
 
     // init our buffer on the GPU
@@ -145,12 +151,19 @@ int main(void)
     // set our new buffer as the current buffer
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     // fill our active buffer on the GPU with some data
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 2 * 6 * sizeof(float), positions, GL_STATIC_DRAW);
     // tell our GPU about the structure of our data (cols & rows)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    // set our new buffer as the current buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    // fill our active buffer on the GPU with some data
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -164,7 +177,9 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // When using an index buffer (GL_ELEMENT_ARRAY) you must use
+        // glDrawElements instead of glDrawArrays
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
